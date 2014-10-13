cb2eed83784a29c9785ea50e1e32f019d9ceae7f
Maksym Borysenko <borysenko.maksym@gmail.com>
2014-10-13 17:41:12 +0300

    Moved Dashboard View to separate folder Added main Page View Added reset and normalize styles Implemented layout

--- projects/catalogue/Leviafan/Views/Page/Page.ts
+++ projects/catalogue/Leviafan/Views/Page/Page.ts
@@ -1,11 +1,11 @@
-/// <reference path="../../Common/Library/Core/Types/OO.d.ts" />
-/// <reference path="../../Common/Library/UI/Core/Views/ViewBase.d.ts" />
+/// <reference path="../../../Common/Library/Core/Types/OO.d.ts" />
+/// <reference path="../../../Common/Library/UI/Core/Views/ViewBase.d.ts" />
 
 module LVF.Views
 {
     eval(SDL.Client.Types.OO.enableCustomInheritance);
 
-    export class Dashboard extends SDL.UI.Core.Views.ViewBase
+    export class Page extends SDL.UI.Core.Views.ViewBase
     {
         constructor(element: HTMLElement, settings?: any)
         {
@@ -18,6 +18,6 @@ module LVF.Views
         }
     }
 
-    SDL.Client.Types.OO.createInterface("LVF.Views.Dashboard", Dashboard)
+    SDL.Client.Types.OO.createInterface("LVF.Views.Page", Page)
 }
 
