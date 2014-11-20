31a07428ef33c57e7f3044c50f8112839f318ec4
Maksym Borysenko <borysenko.maksym@gmail.com>
2014-11-20 19:56:17 +0200

    Added culture files for Products page

---
+++ projects/catalogue/Leviafan/Views/Pages/Products/Cultures/Products.js
@@ -0,0 +1,8 @@
+﻿SDL.Globalize.addCultureInfo( "default", {
+    messages: {
+        "products.gridTitles.SKU": "SKU",
+        "products.gridTitles.VENDOR": "Vendor",
+        "products.gridTitles.COLLECTIONS": "Collections",
+        "products.gridTitles.TITLE": "Name"
+    }
+});
---
+++ projects/catalogue/Leviafan/Views/Pages/Products/Cultures/Products.ru.js
@@ -0,0 +1,13 @@
+﻿SDL.Globalize.addCultureInfo( "nl", {
+    messages: {
+        "homePage.welcomeText": "Welkom",
+        "homePage.favoritesTabLabel": "Favorieten",
+        "homePage.search": "Zoeken",
+        "homePage.quickLinks": "Snelkoppelingen",
+        "homePage.quickLinks.gettingStarted": "Startersgids",
+        "homePage.quickLinks.userGuides": "Gebruikershandleidingen",
+        "homePage.quickLinks.configurationGuides": "Configuratie handleidingen",
+        "homePage.quickLinks.apiReference": "API Referentie",
+        "homePage.quickLinks.installationGuides": "Installatie handleidingen"
+    }
+});
