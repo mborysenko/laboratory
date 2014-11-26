46e0e3c65ad2269673fde7307f717b67693336c2
Maksym Borysenko <borysenko.maksym@gmail.com>
2014-11-26 20:04:48 +0200

    Added a ContentPage control Added styles

---
+++ projects/catalogue/Leviafan/Controls/ContentPage/ContentPage.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../../Common/Library/Core/Types/OO.d.ts" />
+/// <reference path="../../../Common/Library/UI/Core/Views/ViewBase.d.ts" />
+/// <reference path="../../../Common/Library/UI/Core/Controls/ControlBase.d.ts" />
+/// <reference path="../../../Common/Library/UI/Core.Knockout/Libraries/knockout/knockout.d.ts" />
+/// <reference path="../../../Common/Library/Core/Libraries/jQuery/jQuery.d.ts" />
+/// <reference path="../../../Common/Library/Core/Libraries/jQuery/SDL.jQuery.d.ts" />
+
+module LVF.Controls{
+    eval(SDL.Client.Types.OO.enableCustomInheritance);
+
+    export class ContentPage extends SDL.UI.Core.Controls.ControlBase
+    {
+        constructor(element: HTMLElement, options?: any)
+        {
+            super(element, options);
+        }
+
+        public render(callback?: () => void, errorcallback?: (error: string) => void): void
+        {
+            alert("SDL.UI.Core.Controls.ControlBase rendered");
+            this.callBase("SDL.UI.Core.Controls.ControlBase", "render", arguments);
+        }
+        public update(options: any): void
+        {
+            console.log(options);
+            this.callBase("SDL.UI.Core.Controls.ControlBase", "update", arguments);
+        }
+    }
+
+    SDL.Client.Types.OO.createInterface("LVF.Controls.ContentPage", ContentPage);
+}
---
+++ projects/catalogue/Leviafan/Controls/Knockout/ContentPage.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../../Common/Library/UI/Core.Knockout/Controls/Base.d.ts" />
+/// <reference path="../ContentPage/ContentPage.ts" />
+SDL.UI.Core.Knockout.Controls.createKnockoutBinding(LVF.Controls.ContentPage, "LVF.Controls.Knockout.ContentPage");
