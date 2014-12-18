0bc52cc7e31ad542ad53862b4780197fd40d53ea
Maksym <mborysenko@sdl.com>
2014-12-18 21:08:03 +0200

    Added virtual storage of demo data with some API

--- projects/catalogue/Leviafan/Models/Factory.ts
+++ projects/catalogue/Leviafan/Models/Factory.ts
@@ -47,34 +47,67 @@ module LVF.Models
 
     export var Factory = model;
 
-    export var ProductData = {
-        "101": {
-            "sku": "101",
-            "stock": "15",
-            "category": {"id": "201", "title": "Fishing Lines"},
-            "visibility": "true",
-            "vendor": "Drennan",
-            "title": "Drennan Double Strength",
-            "description": "<p><strong>Drennan Double Strength is the first Japanese hi-tech line made widely avail­able in the UK.<strong></p><p>It offers excel­lent strength to dia­meter ratios in a wide range of breaking strains. This soft and supple, neutral tone line behaves and handles very well in a wide range of uses.</p>"
+    export var ProductData: any = {
+        database: {
+            products: [
+                {
+                    id: "101",
+                    sku: "101",
+                    stock: 15,
+                    categoryId: 201,
+                    visibility: true,
+                    vendor: "Drennan",
+                    name: "Drennan Double Strength",
+                    description: "<p><strong>Drennan Double Strength is the first Japanese hi-tech line made widely avail­able in the UK.<strong></p><p>It offers excel­lent strength to dia­meter ratios in a wide range of breaking strains. This soft and supple, neutral tone line behaves and handles very well in a wide range of uses.</p>"
+                },
+                {
+                    id: "102",
+                    sku: "102",
+                    stock: 7,
+                    categoryId: 201,
+                    visibility: "true",
+                    vendor: "Drennan",
+                    name: "Drennan Feeder Braid",
+                    description: "<p>pecially formulated for feeder fishing, Drennan Feeder Braid is a Dyneema-based reel line with an exceptional high breaking strain and knot strength.</p>"
+                },
+                {
+                    id: "103",
+                    sku: "103",
+                    stock: "29",
+                    categorId: 201,
+                    visibility: "true",
+                    vendor: "Drennan",
+                    name: "Drennan Float Fish",
+                    description: "<p><strong>Drennan Float Fish</strong> is an excel­lent main line for all types of float fishing.</p>"
+                }
+            ],
+            categories: [
+                {
+                    id: "201",
+                    name: "Fishing Lines"
+                }
+            ]
         },
-        "102": {
-            "sku": "102",
-            "stock": "7",
-            "category": {"id": "201", "title": "Fishing Lines"},
-            "visibility": "true",
-            "vendor": "Drennan",
-            "title": "Drennan Feeder Braid",
-            "description": "<p>pecially formulated for feeder fishing, Drennan Feeder Braid is a Dyneema-based reel line with an exceptional high breaking strain and knot strength.</p>"
+        getDataByTableName: function(table: string): string
+        {
+            return this.database[table];
         },
-        "103": {
-            "sku": "103",
-            "stock": "29",
-            "category": {"id": "201", "title": "Fishing Lines"},
-            "visibility": "true",
-            "vendor": "Drennan",
-            "title": "Drennan Float Fish",
-            "description": "<p><strong>Drennan Float Fish</strong> is an excel­lent main line for all types of float fishing.</p>"
+        getDataAsString: function(table: string): string
+        {
+            return JSON.stringify(this.database[table]);
+        },
+        getDataItemAsString: function(table: string, id: number): string
+        {
+            var tableData: any[] = this.getDataByTableName(table);
+            var result = null;
+            SDL.jQuery.each(tableData, function(key, item){
+                if(item.id == id)
+                {
+                    result = item;
+                }
+            });
+            return JSON.stringify(result);
         }
-    }
+    };
 }
 
