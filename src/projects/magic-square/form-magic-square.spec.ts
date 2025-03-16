f494c095f246cc95db2f3b5cf025f9fd92245d08
Maksym Borysenko <borysenko.maksym@gmail.com>
2025-03-16 18:02:45 +0100

    fix: broken test

--- src/projects/magic-square/form-magic-square.spec.ts
+++ src/projects/magic-square/form-magic-square.spec.ts
@@ -2,7 +2,11 @@ import { formMagicSquare } from "./form-magic-square";
 
 describe('formMagicSquare', () => {
     it('should return cost of the conversion', () => {
-        const cost = formMagicSquare(`3 6 9\n7 2 6\n4 9 1`);
+        const cost = formMagicSquare([
+            [3, 6, 9],
+            [7, 2, 6],
+            [4, 9, 1]
+        ]);
 
         expect(cost).toBe(20);
     })
